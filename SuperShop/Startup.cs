b4d78e1fc94b16facb8c4d622a487370481047cd
Basteras <mreis822@gmail.com>
2024-07-18 16:42:54 +0100

    First Controller

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SuperShop.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,13 @@ namespace SuperShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Creates the service and injects it the created DataContext
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                //Configures it to usse sql server with the Connections string
+                cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddControllersWithViews();
         }
 
