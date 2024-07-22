70b882d1cf71c1defbc155edf56a85f2094eb9eb
Basteras <mreis822@gmail.com>
2024-07-22 14:50:13 +0100

    Modify products.

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -10,6 +10,10 @@ namespace SuperShop.Data.Entities
         //if the primary key has a different name use this
         //[Key]
         public int Id { get; set; }
+
+        //sets as mandatory
+        [Required]
+        [MaxLength(50,ErrorMessage ="The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -18,11 +22,12 @@ namespace SuperShop.Data.Entities
         [Display(Name = "Image")]
         public string ImageUrl { get; set; }
 
+        //the ? sets this data types as optional
         [Display(Name = "last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
