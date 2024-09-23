eab580e131d2178d6566058132c34eae376bac35
Basteras <mreis822@gmail.com>
2024-09-23 19:42:50 +0100

    Added mobile login page and product page

---
+++ SuperShop.Prism/SuperShop.Prism/Models/UserResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperShop.Prism.Models
+{
+   public class UserResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedEmail {  get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed {  get; set; }
+        public string PasswordHash {  get; set; }
+        public string SecurityStamp { get; set; }
+        public string ConcurrencyStamp { get; set; }
+        public object PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnable {  get; set; }
+        public object LockOutEnd {  get; set; }
+        public object LockOutEnable { get; set; }
+        public int AccessFailedCount { get; set; }
+
+
+
+
+    }
+}
---
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SuperShop.Prism.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnable;
+        private DelegateCommand _loginCommand;
+
+        public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+
+            Title = "Login";
+            IsEnable = true;
+
+
+        }
+        
+        //if is not instantiated, it will be created, this is called a delegate command
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand =new DelegateCommand(Login));
+
+  
+
+        public string Email {  get; set; }
+
+        public string Password {
+            get => _password;
+            set => SetProperty(ref _password, value); 
+        }
+
+        public bool IsRunning
+        {
+            get=> _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnable
+        {
+            get => _isEnable;
+            set=> SetProperty(ref _isEnable, value);
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email","Accept");
+                Password=string.Empty;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an password", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            await App.Current.MainPage.DisplayAlert("Ok", "Welcome", "Accept");
+        }
+    }
+}
