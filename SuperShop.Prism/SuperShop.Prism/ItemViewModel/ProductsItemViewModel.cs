58d0e7e7dfa5b5673ee279038ab2f9e598a18b42
Basteras <mreis822@gmail.com>
2024-09-24 17:01:37 +0100

    Products details page

---
+++ SuperShop.Prism/SuperShop.Prism/ItemViewModel/ProductsItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using SuperShop.Prism.Models;
+using SuperShop.Prism.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperShop.Prism.ItemViewModel
+{
+    public class ProductsItemViewModel : ProductResponse
+    {
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectProductCommand;
+        public ProductsItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+
+        public DelegateCommand SelectProductCommand => _selectProductCommand ?? (_selectProductCommand = new DelegateCommand(SelectProductAsync));
+        private async void SelectProductAsync()
+        {
+            NavigationParameters parameters = new NavigationParameters
+            {
+                {"product",this}
+            };
+            await _navigationService.NavigateAsync(nameof(ProductDetailPage), parameters);
+        }
+    }
+}
